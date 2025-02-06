a44b122d1d54086d197e019949f892c8d5da24cb
nikitaSB01 <118895582+nikitaSB01@users.noreply.github.com>
2025-02-06 19:10:18 +0500

    правка

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import "./App.css";
 /* import List from "./components/List";
  */ import { withHighlight } from "./components/withHighlight";
